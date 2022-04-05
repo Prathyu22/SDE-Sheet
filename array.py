8760b7dfcf1f904ff58967d375043e86a261efa2
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-04-05 12:18:51 +0530

    Triplet_sum_in_array

---
+++ array.py
@@ -0,0 +1,51 @@
+#https://practice.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1
+
+class Solution:
+     
+    #Function to find if there exists a triplet in the 
+    #array A[] which sums up to X.
+    def find3Numbers(self,A, n, X):
+        # Your Code Here
+        A.sort()
+        #fix the first element one by one and find the other 2 elements....
+        for i in range(0, n-2):
+            l = i+1
+            r = n-1
+            while(l<r):
+                if ((A[i]+A[l]+A[r]) == X):
+                    return 1
+                elif ((A[i]+A[l]+A[r]) < X):
+                    l += 1
+                else:
+                    r -= 1
+        return 0
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n,X=map(int,input().strip().split())
+        A=list(map(int,input().strip().split()))
+        ob=Solution()
+        if(ob.find3Numbers(A,n,X)):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
