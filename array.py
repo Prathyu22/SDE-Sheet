c317707d2f7a17cf2fd5a34ae8bbf6dbfbc6dbfa
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-04-05 15:02:46 +0530

    Trapping_rain_water

--- array.py
+++ array.py
@@ -1,51 +1,62 @@
-#https://practice.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1
+#https://practice.geeksforgeeks.org/problems/trapping-rain-water-1587115621/1#
 
-class Solution:
+        #Code here
+        # initialize output
+        result = 0
      
-    #Function to find if there exists a triplet in the 
-    #array A[] which sums up to X.
-    def find3Numbers(self,A, n, X):
-        # Your Code Here
-        A.sort()
-        #fix the first element one by one and find the other 2 elements....
-        for i in range(0, n-2):
-            l = i+1
-            r = n-1
-            while(l<r):
-                if ((A[i]+A[l]+A[r]) == X):
-                    return 1
-                elif ((A[i]+A[l]+A[r]) < X):
-                    l += 1
+        # maximum element on left and right
+        left_max = 0
+        right_max = 0
+     
+        # indices to traverse the array
+        lo = 0
+        hi = n-1
+     
+        while(lo <= hi): 
+            if(arr[lo] < arr[hi]):
+                if(arr[lo] > left_max):
+                    # update max in left
+                    left_max = arr[lo]
                 else:
-                    r -= 1
-        return 0
+                    # water on curr element = max - curr
+                    result += left_max - arr[lo]
+                lo+= 1
+        
+            else:
+                if(arr[hi] > right_max):
+                    # update right maximum
+                    right_max = arr[hi]
+                else:
+                    result += right_max - arr[hi]
+                hi-= 1
+        
+        return result
 
 #{ 
 #  Driver Code Starts
 #Initial Template for Python 3
 
-import atexit
-import io
-import sys
+import math
 
-_INPUT_LINES = sys.stdin.read().splitlines()
-input = iter(_INPUT_LINES).__next__
-_OUTPUT_BUFFER = io.StringIO()
-sys.stdout = _OUTPUT_BUFFER
 
-@atexit.register
 
-def write():
-    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            obj = Solution()
+            print(obj.trappingWater(arr,n))
+            
+            
+            T-=1
 
-if __name__=='__main__':
-    t = int(input())
-    for i in range(t):
-        n,X=map(int,input().strip().split())
-        A=list(map(int,input().strip().split()))
-        ob=Solution()
-        if(ob.find3Numbers(A,n,X)):
-            print(1)
-        else:
-            print(0)
+
+if __name__ == "__main__":
+    main()
+
+
+
 # } Driver Code Ends
