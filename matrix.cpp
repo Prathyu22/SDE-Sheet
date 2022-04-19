ddea53a4f270a5f975418d322667b62d9711db5d
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-04-19 07:20:37 +0530

    Search_a_2D_matrix

--- matrix.cpp
+++ matrix.cpp
@@ -1,74 +1,21 @@
-//https://practice.geeksforgeeks.org/problems/spirally-traversing-a-matrix-1587115621/1
+//https://leetcode.com/problems/search-a-2d-matrix/
 
-#include <bits/stdc++.h> 
-using namespace std; 
-
- // } Driver Code Ends
-class Solution
-{   
-    public: 
-    //Function to return a list of integers denoting spiral traversal of matrix.
-    vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
-    {
-        // code here
-        vector <int> ans;
-        int top = 0, left = 0; //top and left pointers
-        int down = matrix.size()-1, right = matrix[0].size()-1;
-        int dir = 0; // direction pointer
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int r = matrix.size(); // row length
+        int c = matrix[0].size(); // column length
+        int low = 0;
+        int high = (r*c)-1;
+        int mid;
         
-        while(top<=down && left<=right)
+        while(low<=high)
         {
-            if(dir == 0) 
-            {
-                for(int i=left; i<=right; i++) ans.push_back(matrix[top][i]);
-                top++;
-            }
-            else if(dir == 1)
-            {
-                for(int i=top; i<=down; i++) ans.push_back(matrix[i][right]);
-                right--;
-            }
-            else if(dir == 2)
-            {
-                for(int i=right; i>=left; i--) ans.push_back(matrix[down][i]);
-                down--;
-            }
-            else if(dir == 3)
-            {
-                for(int i=down; i>=top; i--) ans.push_back(matrix[i][left]);
-                left++;
-            }
-            dir = (dir+1) % 4;
+            mid = low + (high-low)/2;
+            if(matrix[mid/c][mid%c] == target) return true;
+            else if(matrix[mid/c][mid%c] < target) low = mid+1;
+            else high = mid-1;
         }
-        return ans;
+        return false;
     }
 };
-
-// { Driver Code Starts.
-int main() {
-    int t;
-    cin>>t;
-    
-    while(t--) 
-    {
-        int r,c;
-        cin>>r>>c;
-        vector<vector<int> > matrix(r); 
-
-        for(int i=0; i<r; i++)
-        {
-            matrix[i].assign(c, 0);
-            for( int j=0; j<c; j++)
-            {
-                cin>>matrix[i][j];
-            }
-        }
-
-        Solution ob;
-        vector<int> result = ob.spirallyTraverse(matrix, r, c);
-        for (int i = 0; i < result.size(); ++i)
-                cout<<result[i]<<" ";
-        cout<<endl;
-    }
-    return 0;
-}  // } Driver Code Ends
