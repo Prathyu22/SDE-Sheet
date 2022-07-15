40f6af54ced0209c4f1cedd09bd8552644ff2de4
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-07-15 10:12:11 +0530

    min_no.of_swaps_and_elements_<=k_together

--- array.cpp
+++ array.cpp
@@ -1,48 +1,77 @@
-//https://practice.geeksforgeeks.org/problems/smallest-subarray-with-sum-greater-than-x5651/1
+//https://practice.geeksforgeeks.org/problems/minimum-swaps-required-to-bring-all-elements-less-than-or-equal-to-k-together4847/1#
 
+// C++ program to find minimum swaps required
+// to club all elements less than or equals
+// to k together
 #include <bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
-class Solution{
-  public:
 
-    int smallestSubWithSum(int arr[], int n, int x)
-    {
-        // Your code goes here  
-        int min_len = n;
-        int sum = 0;
-        int i = 0, j = 0;
+ // } Driver Code Ends
+
+
+class Solution
+{
+public:
+    int minSwap(int arr[], int n, int k) {
+        // Complete the function
+        int total = 0; // no. of elements <= K
+        int swap_count = 0;
         
-        for(j=0; j<n; j++)
+        for(int i=0; i<n; i++)
         {
-            sum = sum + arr[j];
-            while(sum > x)
-            {
-                min_len = min(min_len, j-i+1);
-                sum = sum - arr[i++];
-            }
+            if(arr[i] <= k)
+              total++;
         }
-        return min_len;
+        
+        // first sliding window
+        for(int i=0; i<total; i++)
+        {
+            if(arr[i] > k)
+              swap_count++;
+        }
+        
+        //leaving one element from left and 
+        //adding one element in the right part like a sliding window.
+        int i = 0;
+        int j = total;
+        int min_swaps = swap_count; // "min" holds min no. of swaps.
+        while(j<n)
+        {
+            if(arr[i] > k)
+              swap_count--;
+            
+            if(arr[j] > k)
+              swap_count++;
+            
+            min_swaps = min(min_swaps,swap_count);
+            
+            i++;
+            j++;
+        }
+        return min_swaps;
     }
 };
 
+
 // { Driver Code Starts.
 
+// Driver code
 int main() {
-	// your code goes here
-	int t;
+
+	int t,n,k;
 	cin>>t;
 	while(t--)
-	{
-		int n,x;
-		cin>>n>>x;
-		int a[n];
-		for(int i=0;i<n;i++)
-		cin>>a[i];
-		Solution obj;
-		cout<<obj.smallestSubWithSum(a,n,x)<<endl;
-	}
+    {
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        cin>>k;
+        Solution ob;
+        cout << ob.minSwap(arr, n, k) << "\n";
+    }
 	return 0;
-}  // } Driver Code Ends
+}
+  // } Driver Code Ends
