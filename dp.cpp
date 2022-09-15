c12dbe45d00a9461732617f5850bdfd0bd8669ca
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2022-09-15 23:45:54 +0530

    Frog Jump

--- dp.cpp
+++ dp.cpp
@@ -1,71 +1,33 @@
-//https://leetcode.com/problems/climbing-stairs/
+//https://www.codingninjas.com/codestudio/problems/frog-jump_3621012?leftPanelTab=0
 
-/*
-You are climbing a staircase. It takes n steps to reach the top.
-
-Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-
-Example 1:
-
-Input: n = 2
-Output: 2
-Explanation: There are two ways to climb to the top.
-1. 1 step + 1 step
-2. 2 steps
-
-
-Example 2:
-
-Input: n = 3
-Output: 3
-Explanation: There are three ways to climb to the top.
-1. 1 step + 1 step + 1 step
-2. 1 step + 2 steps
-3. 2 steps + 1 step
- 
-
-Constraints:
-1 <= n <= 45 
-
-*/
-
-class Solution {
-public:
-    int climbStairs(int n) {
-/*-------------Recursion (TLE).-----------------*/
-        /*int mk(int n)
-        {
-           if(n<0) return 0;
-           if(n==0) return 1;
-           return mk(n-1)+mk(n-2); 
-        } 
-        int climbStairs(int n) {
-           return mk(n);
-        } */
-        
-/*--------------memoization of recursion solution.-----------*/
-        /*int mk(int n,vector<int> &memo)
-        {     
-           if(n<0) return 0;
-           if(memo[n]!=-1) return memo[n];
-           if(n==0) return 1;
-           return memo[n]=mk(n-1,memo)+mk(n-2,memo);
-        }
-     
-        int climbStairs(int n) {
-           vector<int> memo(n+1,-1);
-           return mk(n,memo);
-        } */
-        
-/*----------------memoization to DP. -----------------*/
-        if(n < 1) return 0;
-        if(n == 1) return 1;
-        int dp[100];
-        dp[0] = 1;
-        dp[1] = 2;
-        for(int i=2; i<n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n-1];
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    /* 
+    //DYNAMIC PROGRAMMING WITH TC = O(N) AND SC = O(N).
+    vector<int> dp(n, -1);
+    int fs, ss = 1001;
+    dp[0] = 0;
+    for(int i=1; i<=n-1; i++){
+        fs = dp[i-1] + abs(heights[i] - heights[i-1]);
+        if(i > 1)
+            ss = dp[i-2] + abs(heights[i] - heights[i-2]);
+       dp[i] = min(fs, ss);
     }
-};
+    return dp[n-1]; */
+    
+    // DP with TC = O(N) and SC = O(1)
+    
+    int prev1 = 0;
+    int prev2 = 0;
+    int fs, ss = 1001;
+    for(int i=1; i<=n-1; i++){
+        fs = prev1 + abs(heights[i] - heights[i-1]);
+        if(i > 1)
+            ss = prev2 + abs(heights[i] - heights[i-2]);
+        int curr = min(fs, ss);
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
+}
